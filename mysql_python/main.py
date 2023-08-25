33b99ff277c96b93e38ab93635d5e4ee2853af81
Cristian Freite <evasquez15@cuc.edu.co>
2023-08-25 15:09:22 -0500

    'insert_values' refactor and 'with an as' added

--- mysql_python/main.py
+++ mysql_python/main.py
@@ -1,14 +1,5 @@
 import mysql.connector
 
-def insertValues(table:str, columns:str, values:str):
-    mycursor.execute("INSERT INTO "+table+"("+columns+") VALUES("+values+")")
-
-def querry(query:str):
-    mycursor.execute(query)
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
-
 try:
     mydb = mysql.connector.connect(
         host="localhost",
@@ -16,10 +7,23 @@ try:
         password="1234",
         database="mydb")
     mycursor = mydb.cursor()
-except:
-    print("connection error")
+except mysql.connector.Error as err:
+    print("Connection error:", err)
 
-insertValues("asignatura","idAsignatura,Notas_idNotas,Notas_Notas_idNotas","01001,02323,12312")
-querry("SELECT * FROM asignatura")
+def insert_values(table, columns, values):
+    try:
+        sql = "INSERT INTO {} ({}) VALUES ({})".format(table, columns, values)
+        mycursor.execute(sql)
+        mydb.commit()
+    except mysql.connector.Error as err:
+        print("Insertion error:", err)
 
-mydb.close()
+def query(query):
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+with mydb as connection:
+    insert_values("asignatura", "idAsignatura, Notas_idNotas, Notas_Notas_idNotas", "01001, 02323, 12312")
+    query("SELECT * FROM asignatura")
